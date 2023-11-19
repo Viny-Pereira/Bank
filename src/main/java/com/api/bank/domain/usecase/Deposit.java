885d5cf1ff9e4303bd3735b46d0db8fa3c06da29
Viny-Pereira <vinypereira75@gmail.com>
2023-11-19 16:19:02 -0300

    :sparkles: deposit

---
+++ src/main/java/com/api/bank/domain/usecase/Deposit.java
@@ -0,0 +1,27 @@
+package com.api.bank.domain.usecase;
+
+import com.api.bank.domain.gateway.AccountGateway;
+import com.api.bank.domain.model.Account;
+
+import java.math.BigDecimal;
+import java.math.RoundingMode;
+
+public class Deposit {
+
+    private final AccountGateway accountGateway;
+
+    public Deposit(AccountGateway accountGateway) {
+        this.accountGateway = accountGateway;
+    }
+    public BigDecimal execute(Account account, BigDecimal amount) throws Exception {
+        amount = amount.setScale(2, RoundingMode.HALF_UP);
+        Account existingAccount = accountGateway.searchByCpf(account.getCpf());
+        if (existingAccount==null){
+            throw new Exception("Account not find in our database");
+        }
+        BigDecimal newBalance = account.getBalance().add(amount);
+        account.setBalance(newBalance);
+        accountGateway.updateAccount(account);
+        return account.getBalance();
+    }
+}
