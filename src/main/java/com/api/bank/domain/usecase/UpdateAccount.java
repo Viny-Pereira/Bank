471a348c9538a5a8176a20e8376191923b1acac6
Viny-Pereira <vinypereira75@gmail.com>
2023-11-19 15:27:56 -0300

    :sparkles: impl update

---
+++ src/main/java/com/api/bank/domain/usecase/UpdateAccount.java
@@ -0,0 +1,23 @@
+package com.api.bank.domain.usecase;
+
+import com.api.bank.domain.gateway.AccountGateway;
+import com.api.bank.domain.model.Account;
+
+public class UpdateAccount {
+    private final AccountGateway accountGateway;
+
+    public UpdateAccount(AccountGateway accountGateway) {
+        this.accountGateway = accountGateway;
+    }
+
+    public void execute(Account updateAccount) throws Exception {
+        Account existingAccount = accountGateway.searchByCpf(updateAccount.getCpf());
+        // validar se o usuario ja possui uma conta
+        if(existingAccount==null) {
+            // - se possuir vamos lancar uma exception
+            throw new Exception("User not found");
+        }
+        accountGateway.updateAccount(updateAccount);
+
+    }
+}
