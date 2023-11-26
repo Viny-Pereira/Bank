f3b6191da26f95c90f3d586db462364aefd9c71f
Viny-Pereira <vinypereira75@gmail.com>
2023-11-25 21:49:17 -0300

    :sparkles: list all

---
+++ src/main/java/com/api/bank/domain/usecase/ListAllAccount.java
@@ -0,0 +1,19 @@
+package com.api.bank.domain.usecase;
+
+import com.api.bank.domain.gateway.interfaces.AccountGateway;
+import com.api.bank.domain.model.Account;
+
+import java.util.List;
+
+public class ListAllAccount {
+    private final AccountGateway accountGateway;
+
+    public ListAllAccount(AccountGateway accountGateway) {
+        this.accountGateway = accountGateway;
+    }
+
+    public List<Account> execute(){
+        return accountGateway.getAll();
+    }
+
+}
